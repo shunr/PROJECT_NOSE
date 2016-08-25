2e1748ead6caf9a4a026a40f3d9abeef7009e5d6
Shun Yu Rao <shunyu.rao@gmail.com>
2016-08-25 12:33:13 -0400

    update logger

--- service/logger.py
+++ service/logger.py
@@ -4,7 +4,7 @@ import sys
 def init(name):
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
-    handler = logging.FileHandler('log.txt', mode='w')
+    handler = logging.FileHandler('service.log', mode='w')
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
